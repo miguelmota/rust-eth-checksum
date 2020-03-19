c84497986855dc9f53d155ef84418e2a29c74f85
Lachezar Lechev <elpiel93@gmail.com>
2020-03-19 10:24:03 +0200

    remove zip and use range indexing to avoid String allocation - also wrap the hashing in `address_hash`

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,17 @@ use crypto::{digest::Digest, sha3::Sha3};
 pub fn checksum(address: &str) -> String {
     let address = address.trim_start_matches("0x").to_lowercase();
 
-    let mut hasher = Sha3::keccak256();
-    hasher.input_str(&address);
-    let address_hash = hasher.result_str();
+    let address_hash = {
+        let mut hasher = Sha3::keccak256();
+        hasher.input_str(&address);
+        hasher.result_str()
+    };
 
-    address.chars().zip(address_hash.chars()).fold(
+    address.chars().enumerate().fold(
         String::from("0x"),
-        |mut acc, (address_char, hash_char)| {
-            let n = u16::from_str_radix(&hash_char.to_string(), 16).unwrap();
+        |mut acc, (index, address_char)| {
+            // this cannot fail since it's Keccak256 hashed
+            let n = u16::from_str_radix(&address_hash[index..index+1], 16).unwrap();
 
             if n > 7 {
                 // make char uppercase if ith character is 9..f
